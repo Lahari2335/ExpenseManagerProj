d939b7c9af604fc4afc6564d0ed25875ef417cbf
Lahari2335 <laharigudla306@gmail.com>
2025-10-24 14:20:29 +0530

    SpringBootProjectApplication.java

--- EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/SpringBootProjectApplication.java
+++ EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/SpringBootProjectApplication.java
@@ -2,10 +2,10 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class SpringBootProjectApplication extends SpringBootServletInitializer
+public class SpringBootProjectApplication 
 {
 	
 	public static void main(String[] args) {
