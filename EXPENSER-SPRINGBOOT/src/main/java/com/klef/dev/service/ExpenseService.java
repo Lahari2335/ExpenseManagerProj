32ae3f0b77af561d3b1042178cbcf2bdd4f59488
Lahari2335 <laharigudla306@gmail.com>
2025-10-24 14:19:33 +0530

    Update ExpenseService.java

--- EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/service/ExpenseService.java
+++ EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/service/ExpenseService.java
@@ -1,30 +1,12 @@
 package com.klef.dev.service;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.klef.dev.entity.Expense;
-import com.klef.dev.repository.ExpenseRepository;
 
-@Service  // <-- THIS IS REQUIRED
-public class ExpenseService {
-
-    @Autowired
-    private ExpenseRepository expenseRepository;
-
-    public List<Expense> getAllExpenses() {
-        return expenseRepository.findAll();
-    }
-
-    public Expense addExpense(Expense expense) {
-        return expenseRepository.save(expense);
-    }
-
-    public Expense updateExpense(Expense expense) {
-        return expenseRepository.save(expense);
-    }
-
-    public void deleteExpense(int id) {
-        expenseRepository.deleteById(id);
-    }
+public interface ExpenseService {
+    Expense addExpense(Expense expense);
+    List<Expense> getAllExpenses();
+    Expense updateExpense(Expense expense);
+    void deleteExpense(int id);
+    List<Expense> searchExpenses(String keyword);
 }
