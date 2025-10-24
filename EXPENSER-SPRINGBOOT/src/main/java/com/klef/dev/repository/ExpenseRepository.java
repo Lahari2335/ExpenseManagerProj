b4c8bd7e8572a665adc62495f354f0ae19aecee3
Lahari2335 <laharigudla306@gmail.com>
2025-10-24 14:18:58 +0530

    ExpenseRepository.java

--- EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/repository/ExpenseRepository.java
+++ EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/repository/ExpenseRepository.java
@@ -1,7 +1,15 @@
 package com.klef.dev.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.klef.dev.entity.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
+
+    @Query("SELECT e FROM Expense e WHERE LOWER(e.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+           "OR LOWER(e.category) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+           "OR LOWER(e.notes) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Expense> searchExpenses(@Param("keyword") String keyword);
 }
