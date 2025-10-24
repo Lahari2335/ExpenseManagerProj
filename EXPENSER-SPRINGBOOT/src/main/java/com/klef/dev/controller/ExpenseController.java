d6a0819123fe017dc325d65bc8c3b22b9a9a823b
Lahari2335 <laharigudla306@gmail.com>
2025-10-24 14:17:46 +0530

    Update ExpenseController.java

--- EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/controller/ExpenseController.java
+++ EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/controller/ExpenseController.java
@@ -1,20 +1,23 @@
 package com.klef.dev.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.klef.dev.entity.Expense;
 import com.klef.dev.service.ExpenseService;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/expenseapi")
+@CrossOrigin(origins = "*")
 public class ExpenseController {
 
     @Autowired
     private ExpenseService expenseService;
+
+    @GetMapping("/")
+    public String home() {
+        return "ExpenseManager API Running";
+    }
 
     @GetMapping("/all")
     public List<Expense> getAllExpenses() {
@@ -36,5 +39,10 @@ public class ExpenseController {
     public String deleteExpense(@PathVariable int id) {
         expenseService.deleteExpense(id);
         return "Deleted";
+    }
+
+    @GetMapping("/search")
+    public List<Expense> searchExpenses(@RequestParam("keyword") String keyword) {
+        return expenseService.searchExpenses(keyword);
     }
 }
